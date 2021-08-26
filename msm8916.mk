53af2a17796851eefe6a910eeceb611b7ed65608
David <davidmolinamorales1999@gmail.com>
2021-08-26 19:21:10 +0200

    Update msm8916.mk

--- msm8916.mk
+++ msm8916.mk
@@ -16,8 +16,7 @@
 
 # Inherit from common
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
