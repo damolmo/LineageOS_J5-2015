cc3c960da2de67540e546291fee0658cc2a23137
David <davidmolinamorales1999@gmail.com>
2021-08-26 19:08:43 +0200

    msm8916-common: manifest: add android.hardware.bluetooth{,.a2dp}@1.0

--- msm8916.mk
+++ msm8916.mk
@@ -78,6 +78,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl.mock
 
 PRODUCT_PACKAGES += \
     libbase_shim \
