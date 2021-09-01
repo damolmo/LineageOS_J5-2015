91f06be5ffa8aea0172ef1c0e771426beda1347a
David <davidmolinamorales1999@gmail.com>
2021-09-01 23:36:18 +0200

    msm8916: removing this here to prevent multiple declatations

--- msm8916.mk
+++ msm8916.mk
@@ -18,9 +18,6 @@
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Include proprietary blobs
 $(call inherit-product-if-exists, vendor/samsung/msm8916-common/msm8916-common-vendor.mk)
 
