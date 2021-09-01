2aca21db489ab8c0f4b15f0ff8932a515277a8ab
David <davidmolinamorales1999@gmail.com>
2021-09-01 23:37:04 +0200

    msm8916: adding telephony products

---
+++ device.mk
@@ -0,0 +1,17 @@
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+# Include full languages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
