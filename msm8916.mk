767934a3d663929417aa099978401e26f3b1a238
David <davidmolinamorales1999@gmail.com>
2021-09-06 22:31:51 +0200

    Update msm8916.mk

--- msm8916.mk
+++ msm8916.mk
@@ -345,6 +345,10 @@ PRODUCT_PACKAGES += \
     thermal.msm8916
     
    # Radio
+   PRODUCT_PACKAGES += \
+android.hardware.radio@1.0 \
+android.hardware.radio.config@1.0 \
+android.hardware.radio.config@1.0-service
 DEVICE_MANIFEST_FILE += device/samsung/msm8916-common/treble-manifest-radio.xml
 
 # Soong namespaces
