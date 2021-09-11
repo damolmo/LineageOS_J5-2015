e9a0152ef9db00d35abe06f3175ab2338c145c07
David <davidmolinamorales1999@gmail.com>
2021-09-11 11:43:23 +0200

    msm8916-common: add Radio products

--- msm8916.mk
+++ msm8916.mk
@@ -362,6 +362,11 @@ PRODUCT_PACKAGES += \
     
    # Radio
    PRODUCT_PACKAGES += \
+   android.hardware.radio@1.0-impl \
+    android.hardware.radio@1.0-service \
+    android.hardware.radio@1.2-impl \
+    android.hardware.radio@1.2-service \
+    android.hardware.radio.config@1.0-service
    DEVICE_MANIFEST_FILE += device/samsung/msm8916-common/treble-manifest-radio.xml
  
 # NFC
