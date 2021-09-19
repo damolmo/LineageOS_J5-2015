821f7af85994fad02f542eb03353b4b716b9596c
David <davidmolinamorales1999@gmail.com>
2021-09-19 11:22:07 +0200

    msm8916-common: Use config for better memory optimization

--- msm8916.mk
+++ msm8916.mk
@@ -443,3 +443,11 @@ PRODUCT_PACKAGES += \
     RemoveRecorder \
     RemoveGallery2 \
     RemoveEleven
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
