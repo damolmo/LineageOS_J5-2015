239ed3b6b9efbdc02719cc42f8e6229debccc2ab
David <davidmolinamorales1999@gmail.com>
2021-09-19 11:19:20 +0200

    msm8916-common: update keymaster products && link manifest

--- msm8916.mk
+++ msm8916.mk
@@ -202,7 +202,9 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-android.hardware.keymaster@4.1-service
+   android.hardware.keymaster@3.0-impl \
+   android.hardware.keymaster@3.0-service
+   DEVICE_MANIFEST_FILE += device/samsung/msm8916-common/Manifest/treble-manifest-keymaster.xml
 
 # Lights
 PRODUCT_PACKAGES += \
