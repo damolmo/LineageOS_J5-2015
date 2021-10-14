1318eb30b15d031ecfeb8fedfbe0b62a8a194749
David <davidmolinamorales1999@gmail.com>
2021-10-14 12:53:30 +0200

    S: upgrade keymaster HALs && include makefile for S hidl

--- msm8916.mk
+++ msm8916.mk
@@ -200,9 +200,12 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-   android.hardware.keymaster@3.0-impl \
-   android.hardware.keymaster@3.0-service
+   android.hardware.keymaster@4.0-impl \
+   android.hardware.keymaster@4.0-service
    DEVICE_MANIFEST_FILE += device/samsung/msm8916-common/Manifest/treble-manifest-keymaster.xml
+
+# HIDL HALs
+$(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 
 # Lights
 PRODUCT_PACKAGES += \
