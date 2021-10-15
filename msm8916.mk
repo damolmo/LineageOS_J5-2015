2fcf35cff017093249aaaa437745a374b37b3c1e
David <davidmolinamorales1999@gmail.com>
2021-10-15 14:47:15 +0200

    S: build vendor copy of clearkey

--- msm8916.mk
+++ msm8916.mk
@@ -129,7 +129,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.3.vendor
     
 # Protobuf
 PRODUCT_COPY_FILES += \
