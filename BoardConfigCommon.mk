df52005fd6163690385bc4dae825371c7e199a62
David <davidmolinamorales1999@gmail.com>
2021-10-15 14:28:59 +0200

    S: drop selinux for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,8 +212,8 @@ TARGET_RECOVERY_PIXEL_FORMAT	:= "RGB_565"
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true
 #include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-    BOARD_SEPOLICY_DIRS += \
-    $(COMMON_PATH)/sepolicy/vendor
+#    BOARD_SEPOLICY_DIRS += \
+#   $(COMMON_PATH)/sepolicy/vendor
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
