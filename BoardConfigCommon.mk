34e61bd77443c25a2c4dcd9cf338a47df8c90c3e
David <davidmolinamorales1999@gmail.com>
2021-10-24 12:43:41 +0200

    msm8916-common: change sepolicy path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,7 +218,7 @@ TARGET_RECOVERY_PIXEL_FORMAT	:= "RGB_565"
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true
 #include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
