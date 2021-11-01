28176564ec3c536903281ffcbfdff30a3537c311
David <davidmolinamorales1999@gmail.com>
2021-11-01 12:00:55 +0100

    msm8916-common: revert previous commit
    cbad5c360fe1db3f8ae2a4c3cc68ff2312c507cc

--- msm8916.mk
+++ msm8916.mk
@@ -145,6 +145,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.3.vendor
     
+    
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+    
 # Protobuf
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so \
