3d2eb93e22e532abdbf71e7659cfeebdba3bb64e
David <davidmolinamorales1999@gmail.com>
2021-11-03 00:49:21 +0100

    msm8916-common: restore sepolicy path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,7 +212,7 @@ TARGET_RECOVERY_PIXEL_FORMAT	:= "RGB_565"
 
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true
-#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Shims
